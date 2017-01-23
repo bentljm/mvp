94ffc429baf4aeca297a803cb6dddac36ba8cfdb
Jeff Bentler <bentljm@gmail.com>
2017-01-23 14:00:42 -0800

    Updated timer.html to display three forms for hours, minutes and seconds

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 angular.module('app', ['ngRoute'])
+
   .config(function($routeProvider) {
     $routeProvider
-      .when('/homepage', {
-        templateUrl: 'partial/homepage.html',
-        controller: 'HomepageCtrl'
-      })
       .when('/timer', {
-        templateUrl: 'partial/timer.html',
+        templateUrl: 'partials/timer.html',
         controller: 'TimerCtrl'
       })
-      otherwise({
-        redirectTo: '/homepage'
+      .otherwise({
+        redirectTo: '/'
       });
   })
 
+  .controller('TimerCtrl', function($scope) {
+    $scope.time = 0;
 
-.factory('counter', function() {
 
-})
+  })
 
-.controller('HomepageCtrl', function($scope) {
 
-})
 
-.controller('TimerCtrl', function($scope) {
 
-})
+
+// function getTimeRemaining(endtime){
+//       var t = Date.parse(endtime) - Date.parse(new Date());
+//       var seconds = Math.floor( (t/1000) % 60 );
+//       var minutes = Math.floor( (t/1000/60) % 60 );
+//       var hours = Math.floor( (t/(1000*60*60)) % 24 );
+//       var days = Math.floor( t/(1000*60*60*24) );
+//       return {
+//         'total': t,
+//         'days': days,
+//         'hours': hours,
+//         'minutes': minutes,
+//         'seconds': seconds
+//       };
+//     }
